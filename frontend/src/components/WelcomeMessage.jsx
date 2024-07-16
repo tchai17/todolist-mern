6f5755354d7728490a3482d4c3ddba8340a706b4
Timothy Chai <timothy.chai@fdmgroup.com>
2024-07-16 16:20:21 +0800

    Implement tailwind styles lol

--- frontend/src/components/WelcomeMessage.jsx
+++ frontend/src/components/WelcomeMessage.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function WelcomeMessage() {
   return (
     <div className="welcome-message">
-      <h1>Welcome to your To-Do List</h1>
+      <h1 className="font-sans hover:font-serif text-8xl">
+        Welcome to your To-Do List
+      </h1>
     </div>
   );
 }
