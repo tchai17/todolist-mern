582ea4ed737e43cee4cb27a30139875b139abff7
Timothy Chai <timothy.chai@fdmgroup.com>
2024-07-17 13:42:22 +0800

    Add conditional formatting and rendering depending on whether a task is done or not

--- frontend/src/components/ToDoItem.jsx
+++ frontend/src/components/ToDoItem.jsx
@@ -2,18 +2,32 @@ import React from "react";
 import { TableCell, TableRow } from "./ui/table";
 import { Button } from "@/components/ui/button";
 
-const cellFormat = "border border-slate-300 p-4 text-500 text-lg font-semibold";
+const cellFormat =
+  "border border-slate-300 p-4 text-500 text-lg font-semibold ";
 
 const ToDoItem = ({ description, isDone, createdDate }) => {
+  // conditional shading for tasks that are done
+  const conditionalShade = isDone ? "bg-slate-500 opacity-50" : "";
+
+  // only show done button for tasks that are not done
+  const doneButton = (isDone) => {
+    if (!isDone) {
+      return <Button>Done!</Button>;
+    }
+  };
   return (
     <TableRow>
-      <TableCell className={cellFormat}>{description}</TableCell>
-      <TableCell className={cellFormat}>{isDone ? "Yes" : "No"}</TableCell>
-      <TableCell className={cellFormat}>
+      <TableCell className={cellFormat + conditionalShade}>
+        {description}
+      </TableCell>
+      <TableCell className={cellFormat + conditionalShade}>
+        {isDone ? "Yes" : "No"}
+      </TableCell>
+      <TableCell className={cellFormat + conditionalShade}>
         {createdDate.toLocaleDateString()}
       </TableCell>
-      <TableCell className={cellFormat + " text-center"}>
-        <Button>Done!</Button>
+      <TableCell className={cellFormat + " text-center " + conditionalShade}>
+        {doneButton(isDone)}
       </TableCell>
     </TableRow>
   );
