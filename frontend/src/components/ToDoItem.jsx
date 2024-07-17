5c2401b4ed13e4ace15e2fe72dc9e0633b755a62
Timothy Chai <timothy.chai@fdmgroup.com>
2024-07-17 15:02:28 +0800

    Add PUT request for marking task as done

--- frontend/src/components/ToDoItem.jsx
+++ frontend/src/components/ToDoItem.jsx
@@ -1,18 +1,31 @@
 import React from "react";
+import axios from "axios";
 import { TableCell, TableRow } from "./ui/table";
 import { Button } from "@/components/ui/button";
 
 const cellFormat =
   "border border-slate-300 p-4 text-500 text-lg font-semibold ";
 
-const ToDoItem = ({ description, isDone, createdDate }) => {
+const ToDoItem = ({ _id, description, isDone, createdDate }) => {
   // conditional shading for tasks that are done
   const conditionalShade = isDone ? "bg-slate-500 opacity-50" : "";
 
   // only show done button for tasks that are not done
   const doneButton = (isDone) => {
+    function markAsDone() {
+      axios({
+        method: "PUT",
+        url: "http://localhost:3000/tasks/" + _id,
+        data: {
+          _id: _id,
+          isDone: true,
+          createdDate: createdDate,
+          description: description,
+        },
+      }).catch((error) => console.error("Error marking task as done", error));
+    }
     if (!isDone) {
-      return <Button>Done!</Button>;
+      return <Button onClick={markAsDone}>Done!</Button>;
     }
   };
   return (
