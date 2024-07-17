9142951c0540679b4b29312d1b267c022a66066f
Timothy Chai <timothy.chai@fdmgroup.com>
2024-07-17 13:41:40 +0800

    Reduce size of welcome message

--- frontend/src/components/WelcomeMessage.jsx
+++ frontend/src/components/WelcomeMessage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function WelcomeMessage() {
   return (
     <div className="welcome-message">
-      <h1 className="font-sans text-8xl text-center my-5">
+      <h1 className="font-sans text-5xl text-center my-5">
         Welcome to your To-Do List
       </h1>
     </div>
